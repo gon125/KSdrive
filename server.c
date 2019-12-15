677617d6f789db154d0bfea4fcb20b46dcfdcecd
Hyunsu Jang <vhtmxmqhdl@gmail.com>
2019-12-16 04:08:39 +0900

    update

--- server.c
+++ server.c
@@ -17,6 +17,7 @@ int server_bytes_sent;
 
 void* handle_call(void* fdptr);
 void setup(pthread_attr_t* attrp);
+FILE* p_open(const char* command, const char* mode) ;
 
 int process_rq(int fd) {
  	char id[1000], pwd[1000], fname[1000], buf[1000];	
@@ -26,17 +27,26 @@ int process_rq(int fd) {
 
 	FILE* f;
 	int c = 0, i = 0;
-	char str[1000];
+	char str[1000], cmd[1000];
 
 	while(1){
-		fin = fdopen(dup(fd), "r");
-		fout = fdopen(dup(fd), "w");
-
-		fscanf(fin, "%d",&rq);
-
+		if((fin = fdopen(dup(fd), "r"))==NULL){
+			perror("fin fdopen");
+			exit(1);
+		}
+		if((fout = fdopen(dup(fd), "w"))==NULL){
+			perror("fout fdopen");
+			exit(1);
+		}
+		while(1){
+			c = getc(fin);
+			if ( c - '0'>=0 && c - '0' <=6){
+				rq = c - '0';
+				break;
+			} 
+		}
 		printf("got a call on %d: request = %d\n",fd ,rq);
 		printf("%d\n",rq);
-		fflush(stdout);
 
 		switch(rq){
 			case SIGNUP :
@@ -53,7 +63,6 @@ int process_rq(int fd) {
 				}
 				break;
 			case LOGIN :
-				
 				fscanf(fin, "%s %s",id, pwd);
 				getc(fin);
 				printf("%s %s\n",id ,pwd);
@@ -66,42 +75,51 @@ int process_rq(int fd) {
 				break;
 			case LOGOUT :
 				fprintf(fout, "%d",1);
+				fflush(fout);
 				server_requests--;
 				fclose(fout);
 				fclose(fin);
 				return 0;
 				break;
 			case LS:
-				break;
-			case LOAD:
-
-				fscanf(fin,"%s",fname);
-				printf("%s",fname);
-				c=getc(fin);
-				putc(c,stdout);
-				fflush(stdout);
-				sprintf(str,"./%s/%s",id,fname);
-
-				if((f = fopen(str,"r"))==NULL){
-					fprintf(fout, "%d",0); // dir에 파일이 존재하지 않을 때
-					printf("야\n");
-					fflush(stdout);
-					break;
-				}else {
-					fprintf(fout, "%d",1); // dir에 파일이 존재할 때
-					printf("임마\n");
-					fflush(stdout);
-				} 
-				printf("%s\n",str);
-				fflush(stdout);
+				sprintf(cmd,"ls ./%s",id);
+				
+				f = popen(cmd, "r");
 				
 				if(fout != NULL && f != NULL){
 					while((c=getc(f))!=EOF){
 						putc(c,fout);
+						fflush(fout);
+						if(putc(c,stdout)=='\n')
+							printf("\n");
+					}
+				}
+				fprintf(fout,"\r\n");
+				fflush(fout);
+				pclose(f);
+				break;
+			case LOAD:
+				fscanf(fin,"%s",fname);
+				printf("%s",fname);
+				c=getc(fin);
+				sprintf(str,"./%s/%s",id,fname);
+
+				if((f = fopen(str,"r"))==NULL){
+					fprintf(fout, "%d",0); // dir에 파일이 존재하지 않을 때
+				}else {
+					fprintf(fout, "%d",1); // dir에 파일이 존재할 때
+				} 
+				printf("%s\n",str);
+				
+				if(fout != NULL && f != NULL){
+					while((c=getc(f))!=EOF){
+						putc(c,fout);
+						fflush(fout);
 						putc(c,stdout);
 					}
 				}
-				
+				fprintf(fout,"\r\n");
+				fflush(fout);
 				fclose(f);
 				break;
 			case SAVE:
@@ -131,13 +149,22 @@ int process_rq(int fd) {
 				fprintf(fout, "%d",1);
 				break;
 			case DELETE:
+				fscanf(fin,"%s",fname);
+				getc(fin);
+				sprintf(str,"./%s/%s",id,fname);
+				if(remove(str) == -1){
+					fprintf(fout,"%d",0);
+				}else{
+					fprintf(fout,"%d",1);
+				}
 				break;
 		}
-		fprintf(fout,"\r\n");
 		fflush(fout);
 		fclose(fout);
-
-
+		strcpy(str,"");
+		strcpy(cmd,"");
+		strcpy(fname,"");
+		c = 0;
 		fclose(fin);
 	}
 	return 1;
@@ -187,4 +214,43 @@ void* handle_call(void* fdptr)
 	process_rq(fd);
 	
 	return NULL;
+}
+
+FILE* p_open(const char* command, const char* mode) {
+	int pip[2], pid;
+	int child_end, parent_end;
+		
+	if (*mode == 'r') {
+		parent_end =  0;
+		child_end = 1;
+	}
+	
+	if (*mode == 'w') {
+		parent_end = 1;
+		child_end = 0;
+	}
+	
+	if (pipe(pip) == -1) {
+		perror("pipe"); exit(1);
+	}
+	
+	if ((pid = fork()) == -1) {
+		perror("fork"); exit(1);
+	}
+
+	if (pid > 0) {
+		if (close(pip[child_end]) == -1) { perror("close"); exit(1);}		
+		return fdopen(pip[parent_end], mode);
+	}
+
+	if (pid == 0) {
+		if (close(pip[parent_end]) == -1) {perror("close"); exit(1);}
+		if (dup2(pip[child_end], child_end) == -1) {perror("dup2"); exit(1);}
+		if (close(pip[child_end]) == -1) {perror("close"); exit(1);}
+		execl("/bin/sh","sh", "-c", command, NULL);
+		perror("execl");	
+		exit(1);
+	}
+	perror("unknown");
+	exit(1);
 }
